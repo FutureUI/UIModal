b1f2e781f98d9fa4ac2a6180c44f76d09eb054b1
8264 <zhangbo8264@163.com>
2017-04-02 17:51:09 +0800

    Signed-off-by: 8264 <zhangbo8264@163.com>

--- UI.Modal.js
+++ UI.Modal.js
@@ -4,22 +4,14 @@ var UI = UI || {};
 
 !function(){
 
-	
-	var Modal=function(options){
-		
-		this.opt=Object.assign({
-			callback:function(){},	//回调函数
-			closeMethod:false		//点击遮罩层关闭 true / false
-		},options)
+	window.onload=function(){
 
-		this.el=null;
-		this.callback = this.opt.callback;
-
-		//计算body初始化的paddingRight
-		this.ORIGINAL_BODY_PAD = document.body.style.paddingRight || '';
+		UI.ORIGINAL_BODY_OFX = document.body.style.overflowX || '';
+		UI.ORIGINAL_BODY_OFY = document.body.style.overflowY || '';
+		UI.ORIGINAL_BODY_PAD = document.body.style.paddingRight || '';
 
 		//计算滚动条宽度
-		this.SCROLLBAR_WIDTH=function () {
+		UI.SCROLLBAR_WIDTH=function () {
 		    var div = document.createElement('div');
 		    div.style.cssText = 'position: absolute;top: -9999px;width: 50px;height: 50px;overflow: scroll;'
 		    document.body.appendChild(div);
@@ -29,35 +21,66 @@ var UI = UI || {};
 		}();
 
 	}
+	
+	var Modal=function(options){
+		
+		this.opt=Object.assign({
+			background:'rgba(0,0,0,0)',
+			zIndex:10000,			
+			callback:function(){},	//回调函数
+			closeMethod:false		//点击遮罩层关闭 true / false
+		},options)
+
+		this.el=null;
+		this.callback = this.opt.callback;
+
+		this.style={
+			'position':'fixed',
+			'top':0,
+			'left':0,
+			'right':0,
+			'bottom':0,
+			'overflow':'auto',
+			'zIndex':this.opt.zIndex,
+			'background':this.opt.background
+		}
+		
+
+	}
 
 	Modal.prototype.show=function(obj){
 
-		var me=this;
+		var _this=this;
 
-		console.log(this.el);
 		if(this.el) return false;
+
+		//判断浏览器是否存在滚动条，修正偏移量
 		
-		this.el=document.createElement('div');
-		this.el.className="ui-modal";
-		document.body.appendChild(this.el);
-		
-		if( document.body.className.indexOf('ui-modal-open') == -1 ){
+		if( document.body.querySelectorAll('ui-modal-open').length == 0 ){
 			
 		    //判断浏览器是否存在滚动条，修正偏移量
 			if(document.body.clientWidth<window.innerWidth){
 				var bodyPad = parseInt(getComputedStyle(document.body)['paddingRight'] || 0);
-				document.body.style.paddingRight=bodyPad + this.SCROLLBAR_WIDTH + 'px';
+				document.body.style.paddingRight=bodyPad + UI.SCROLLBAR_WIDTH + 'px';
 	    	}
 
-	    	document.body.classList.add('ui-modal-open');
+	    	document.body.style.overflowX = 'hidden';
+	    	document.body.style.overflowY = 'hidden';
 
 		}
+
+		this.el=document.createElement('div');
+		this.el.className="ui-modal";
+		for(var s in this.style){
+			this.el.style[s]=this.style[s];
+		}
+		document.body.appendChild(this.el);
 
 		//判断关闭方式，设置透明层可点击关闭modal
 		if( this.opt.closeMethod ){
 			this.el.addEventListener('click',function(e){
 				e.stopPropagation();
-				e.target === this && me.hide();
+				e.target === this && _this.hide();
 			})
 	    }
 
@@ -76,8 +99,9 @@ var UI = UI || {};
 			if(el.style.display != 'none') { hasModal=true; return false; }
 		})
 		if(!hasModal){
-			document.body.classList.remove('ui-modal-open');
-			document.body.style.paddingRight=this.ORIGINAL_BODY_PAD;
+			document.body.style.overflowX = UI.ORIGINAL_BODY_OFX;
+			document.body.style.overflowY = UI.ORIGINAL_BODY_OFY;
+			document.body.style.paddingRight = UI.ORIGINAL_BODY_PAD;
 		}
 
 		return this;
@@ -91,5 +115,3 @@ var UI = UI || {};
 
 	window.UI.Modal=Modal;
 }();
-
-
